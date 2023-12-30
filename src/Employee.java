4246c9b2eb9fd5eb956b83e08681f7f12991e05c
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2023-12-30 15:19:10 +0100

    add Employee class

---
+++ src/Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee {
+    private String lastName;
+    private double salary;
+
+    public Employee(String lastName, double salary) {
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+}
